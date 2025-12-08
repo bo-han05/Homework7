d2ae49e58ab72dfec1467254bafb560bf90a2d23
Bo Han <2005bhan@gmail.com>
2025-12-08 17:12:46 -0600

    second commit

--- HW7_api_server.R
+++ HW7_api_server.R
@@ -1,1 +1,40 @@
 ## Write your API endpoints in this file
+
+library(tidyverse)
+library(lubridate)
+library(plumber)
+library(jsonlite)
+
+load("model.RData")
+
+preprocess_data = function(df) {
+  df %>%
+    mutate(
+      appt_time = ymd_hms(appt_time, tz="UTC"),
+      appt_date = as.Date(appt_time),
+      appt_hour = hour(appt_time),
+      appt_day = wday(appt_time, label=T, abbr=T),
+      diff_time = as.numeric(difftime(appt_date, as.Date(appt_made), units="days"))
+    )
+}
+
+#* Predict probability of no-shows
+#* @post /predict_prob
+function(req) {
+  input_df <- jsonlite::fromJSON(req$postBody)
+  if (!is.data.frame(input_df)) input_df <- as.data.frame(input_df)
+  processed <- preprocess_data(input_df)
+  preds <- predict(model, newdata = processed, type = "response")
+  return(as.numeric(preds))
+}
+
+#* Predict no-show class
+#* @post /predict_class
+function(req) {
+  input_df <- jsonlite::fromJSON(req$postBody)
+  if (!is.data.frame(input_df)) input_df <- as.data.frame(input_df)
+  processed <- preprocess_data(input_df)
+  probs <- predict(model, newdata = processed, type = "response")
+  classes <- ifelse(probs >= 0.5, 1, 0)
+  return(as.numeric(classes))
+}
--- HW7_client_test.R
+++ HW7_client_test.R
@@ -1,1 +1,40 @@
 ## Write your client test code here
+
+library(httr)
+library(jsonlite)
+
+input_df = data.frame(
+  appt_time = c("2023-07-01 13:00:00", "2023-07-02 09:30:00"),
+  appt_made = c("2023-06-25", "2023-06-20")
+)
+
+# Serialize the data frame into JSON
+json_input <- toJSON(input_df, dataframe = "rows")
+
+# Test 1: predict_prob endpoint
+prob_response <- POST(
+  url = "http://127.0.0.1:8080/predict_prob",
+  body = json_input,
+  encode = "json",            # tells httr to send JSON
+  content_type_json()         # sets correct header: application/json
+)
+
+# Unserialize / parse the returned JSON into R
+prob_values <- fromJSON(content(prob_response, "text"))
+
+cat("== Predicted Probabilities ==\n")
+print(prob_values)
+
+# Test 2: predict_class endpoint
+class_response <- POST(
+  url = "http://127.0.0.1:8080/predict_class",
+  body = json_input,
+  encode = "json",
+  content_type_json()
+)
+
+# Unserialize the returned JSON
+class_values <- fromJSON(content(class_response, "text"))
+
+cat("\n== Predicted Classes ==\n")
+print(class_values)
